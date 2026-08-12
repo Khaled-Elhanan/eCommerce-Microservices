483be5693d803d4bda232518e8eab86f117a11db
Khaled Elhanan <deathnotecode1@gmail.com>
2026-08-12 08:33:02 +0300

    add cors and swagger

--- 02. Products Microservice/ProductsMicroService.API/Program.cs
+++ 02. Products Microservice/ProductsMicroService.API/Program.cs
@@ -3,6 +3,7 @@ using DataAccessLayer;
 using FluentValidation.AspNetCore;
 using ProductsMicroService.API.APIEndpoints;
 using ProductsMicroService.API.Middleware;
+using System.Text.Json.Serialization;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,13 +16,29 @@ builder.Services.AddControllers();
 //FluentValidations
 builder.Services.AddFluentValidationAutoValidation();
 
+// Add model binder to read values from json to enum
+
+builder.Services.ConfigureHttpJsonOptions(options => options.SerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+
+
+// Add Swagger 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 app.UseExceptionHandlingMiddleware();
 app.UseRouting();
 
+
+// Swagger 
+app.UseSwagger();
+app.UseSwaggerUI();
+
+
+
 //Auth
+app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 
